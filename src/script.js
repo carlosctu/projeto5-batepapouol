ff1a82304db0c6f68d76bd07724d7de449ed73c3
carlosctu <carlosctu94@gmail.com>
2022-06-27 17:24:39 -0300

    Last Feature

--- src/script.js
+++ src/script.js
@@ -2,8 +2,8 @@ let cadastrarNome = prompt("Qual é seu nome?");
 let mensagens = [];
 let cont = 0;
 let cont2 = 0;
-let receiver = "Todos"
-let typeMessage = "message"
+let receiver = "Todos";
+let typeMessage = "message";
 // const message = {}
 login();
 // Login no Batepapo OUL
@@ -16,11 +16,11 @@ function login() {
     nome
   );
   promise.then(() => {
-    setTimeout(searchMessages, 200)
-    setInterval(enableConnection, 5000)
-  })
+    setTimeout(searchMessages, 200);
+    setInterval(enableConnection, 5000);
+  });
   promise.catch(() => {
-    cadastrarNome = prompt("Qual é seu nome?")
+    cadastrarNome = prompt("Qual é seu nome?");
   });
 }
 //---------------------------------------------------------------
@@ -30,7 +30,7 @@ setInterval(searchMessages, 3000);
 function sendMessage() {
   const messageToSend = document.querySelector(".msg");
   if (messageToSend.value == null || messageToSend.value == "") {
-    return
+    return;
   }
   const message = {
     from: cadastrarNome,
@@ -44,11 +44,12 @@ function sendMessage() {
   );
   promise.then(() => {
     document.querySelector(".msg").value = "";
-    searchMessages()
+    searchMessages();
   });
-  promise.catch(() => console.log(`Deu erro na tua mensagem: ${erro.response.status}`));
+  promise.catch(() =>
+    console.log(`Deu erro na tua mensagem: ${erro.response.status}`)
+  );
 }
-
 //---------------------------------------------------------------
 
 // Manter Conexão
@@ -62,11 +63,10 @@ function enableConnection() {
   );
   promisse.then(() => console.log("Conexão estável"));
   promisse.catch(() => {
-    console.log("Conexão encerrada")
-    window.location.reload()
+    console.log("Conexão encerrada");
+    window.location.reload();
   });
 }
-
 //---------------------------------------------------------------
 
 // Procurar Mensagens
@@ -75,16 +75,16 @@ function searchMessages() {
     "https://mock-api.driven.com.br/api/v6/uol/messages"
   );
   promisse.then((dados) => {
-    mensagens = dados.data
-    renderMessages(mensagens)
+    mensagens = dados.data;
+    renderMessages(mensagens);
   });
   promisse.catch(() => console.log("Erro no carregamento das mensages"));
 }
-
+// Renderizar mensagens
 function renderMessages(mensagens) {
   cont++;
   if (cont !== cont2) {
-    document.querySelector(".chat").innerText = ""
+    document.querySelector(".chat").innerText = "";
   }
   let div = document.querySelector(".chat");
   for (let i = 0; i < mensagens.length; i++) {
@@ -93,13 +93,16 @@ function renderMessages(mensagens) {
     } else if (mensagens[i].type == "message") {
       div.innerHTML += `<div class="message"><p class="hour">(${mensagens[i].time})</p>&nbsp&nbsp<p class="user">${mensagens[i].from}</p>&nbsp&nbsp<p>para</p>&nbsp&nbsp<p class="user">${mensagens[i].to}:</p>&nbsp&nbsp<div><p>${mensagens[i].text}</p><div/></div>`;
     } else if (mensagens[i].type == "private_message") {
-      if ((cadastrarNome == mensagens[i].to) ) {
+      if (
+        cadastrarNome == mensagens[i].to ||
+        (cadastrarNome == mensagens[i].from && receiver == mensagens[i].to)
+      ) {
         div.innerHTML += `<div class="private-message"><p class="hour">(${mensagens[i].time})</p>&nbsp&nbsp<p class="user">${mensagens[i].from}</p>&nbsp&nbsp<p>para</p>&nbsp&nbsp<p class="user">${mensagens[i].to}:</p>&nbsp&nbsp<p>${mensagens[i].text}</p></div>`;
       }
     }
     lastMessage();
   }
-  cont2 = cont
+  cont2 = cont;
 }
 function lastMessage() {
   const element = document.querySelector(".chat").lastElementChild;
@@ -107,29 +110,17 @@ function lastMessage() {
     element.scrollIntoView();
   }
 }
-function cleanMessages() {
-  const messages1 = (document.querySelector(`.log-${cont}`));
-  const messages2 = (document.querySelector(`.message-${cont}`));
-  const messages3 = (document.querySelector(`.private-message${cont}`));
-  if (messages1 !== null) {
-    messages1.innerText = ""
-  } else if (messages2 !== null) {
-    messages2.innerText = ""
-  } else if (messages3 !== nul) {
-    messages3.innerText = ""
-  }
-}
 //---------------------------------------------------------------
 // Procurar Participantes
 function findPeople() {
-  const availableUsers = document.querySelector(".available-users")
+  const availableUsers = document.querySelector(".available-users");
   const promisse = axios.get(
     "https://mock-api.driven.com.br/api/v6/uol/participants"
   );
-  promisse.then(dados => {
-    const participants = dados.data
+  promisse.then((dados) => {
+    const participants = dados.data;
     for (let i = 0; i < participants.length; i++) {
-      availableUsers.innerHTML += `<div><button onclick="selectedPerson(this)"><ion-icon name="person-circle"></ion-icon><p>${participants[i].name}</p></button></div>`
+      availableUsers.innerHTML += `<div><button onclick="selectedPerson(this)"><ion-icon name="person-circle"></ion-icon><p>${participants[i].name}</p></button></div>`;
     }
   });
   promisse.catch(() => console.log("Não foi possivel localizar pessoas"));
@@ -137,60 +128,62 @@ function findPeople() {
 //---------------------------------------------------------------
 // Escolher pessoas
 function selectedPerson(person) {
-  console.log(person)
-  console.log(person)
-  let checkSelectedPerson = document.querySelector(".available-users .check")
+  let checkSelectedPerson = document.querySelector(".available-users .check");
   if (checkSelectedPerson == null || checkSelectedPerson == "") {
-    person.innerHTML += `<ion-icon class="check person" name="checkmark-outline" style="color:green"></ion-icon>`
+    person.innerHTML += `<ion-icon class="check person" name="checkmark-outline" style="color:green"></ion-icon>`;
   } else {
-    checkSelectedPerson.remove()
-    person.innerHTML += `<ion-icon class="check person" name="checkmark-outline" style="color:green"></ion-icon>`
+    checkSelectedPerson.remove();
+    person.innerHTML += `<ion-icon class="check person" name="checkmark-outline" style="color:green"></ion-icon>`;
   }
-  const ppl = document.querySelector(".available-users .check").parentNode
-  receiver = ppl.querySelector("p").innerHTML
-  console.log(receiver)
+  const ppl = document.querySelector(".available-users .check").parentNode;
+  receiver = ppl.querySelector("p").innerHTML;
 }
 //---------------------------------------------------------------
 // sideBar
-const sideBar = document.querySelector(".find-users")
-const check = document.querySelector(".check")
-let loadPeople
+const sideBar = document.querySelector(".find-users");
+const check = document.querySelector(".check");
+let loadPeople;
 function showSideBar() {
-  loadPeople = setInterval(findPeople, 10000)
-  sideBar.classList.remove("hidden")
-  document.querySelector("body").classList.add("no-scroll")
+  loadPeople = setInterval(findPeople, 10000);
+  sideBar.classList.remove("hidden");
+  document.querySelector("body").classList.add("no-scroll");
 }
 function closeSideBar() {
-  clearInterval(loadPeople)
-  sideBar.classList.add("hidden")
-  document.querySelector("body").classList.remove("no-scroll")
+  clearInterval(loadPeople);
+  sideBar.classList.add("hidden");
+  document.querySelector("body").classList.remove("no-scroll");
 }
 // Mostrar participantes
-let checkPublic = ""
-let checkPrivate = ""
+let checkPublic = "";
+let checkPrivate = "";
 
 // Logica dos checks do side bar
 function enableCheck(element) {
-  checkPublic = document.querySelector(".public .check")
-  checkPrivate = document.querySelector(".private .check")
+  checkPublic = document.querySelector(".public .check");
+  checkPrivate = document.querySelector(".private .check");
   if (checkPublic == null && checkPrivate == null) {
-    element.innerHTML += `<ion-icon class="check" name="checkmark-outline" style="color:green"></ion-icon>`
-  } else if ((checkPublic !== null && checkPrivate == null)) {
-    checkPublic.remove()
-    typeMessage = "private_message"
-    element.innerHTML += `<ion-icon class="check" name="checkmark-outline" style="color:green"></ion-icon>`
-  } else if ((checkPublic == null && checkPrivate !== null)) {
-    checkPrivate.remove()
-    typeMessage = "message"
-    element.innerHTML += `<ion-icon class="check" name="checkmark-outline" style="color:green"></ion-icon>`
+    element.innerHTML += `<ion-icon class="check" name="checkmark-outline" style="color:green"></ion-icon>`;
+  } else if (checkPublic !== null && checkPrivate == null) {
+    checkPublic.remove();
+    console.log(receiver)
+    if (receiver == "Todos") {
+      typeMessage = "message";
+    } else {
+      typeMessage = "private_message";
+    }
+    element.innerHTML += `<ion-icon class="check" name="checkmark-outline" style="color:green"></ion-icon>`;
+  } else if (checkPublic == null && checkPrivate !== null) {
+    checkPrivate.remove();
+    typeMessage = "message";
+    element.innerHTML += `<ion-icon class="check" name="checkmark-outline" style="color:green"></ion-icon>`;
   }
 }
 //---------------------------------------------------------------
 // Lógica do enter
 document.addEventListener("keypress", function (element) {
   if (element.key === "Enter") {
-    const btn = document.querySelector(".send")
-    btn.click()
+    const btn = document.querySelector(".send");
+    btn.click();
   }
-})
+});
 //---------------------------------------------------------------
